19cd69da5488cc37748acd3f13727d95190a063f
Dylan Burns <65887526+Dylan-Burns@users.noreply.github.com>
2023-12-09 18:21:13 -0800

    Modified code to add Data visualization for ROC Curve

---
+++ main.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_curve, roc_auc_score
+import matplotlib.pyplot as plt
+
+# Step 1: Load and Audit the Training Data
+data = pd.read_csv('features.csv')
+
+# Check for missing values
+if data.isnull().values.any():
+    print("There are missing values in the dataset")
+else:
+    print("No missing values in the dataset")
+
+# Assuming 'Class Label' is equivalent to 'Label' in the R code
+data['Class Label'] = data['Class Label'].map({0: 'Good', 1: 'Bad'})
+
+# One-hot encode categorical variables
+# Replace ['Country'] with the actual categorical column names in your dataset
+data = pd.get_dummies(data, columns=['Country'])
+
+# Step 2: Train the Random Forest Model
+X = data.drop('Class Label', axis=1)
+y = data['Class Label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=123)
+
+model = RandomForestClassifier(n_estimators=1500, max_features=10, random_state=123)
+model.fit(X_train, y_train)
+
+print(model)
+
+# Step 3: Estimate Accuracy
+predictions = model.predict(X_test)
+print(classification_report(y_test, predictions))
+
+# Confusion matrix for the entire data
+predictions_all = model.predict(X)
+print(confusion_matrix(y, predictions_all))
+
+
+# Calculating additional metrics
+total_rows = len(data)
+print(f"Total number of rows in the dataset: {total_rows}")
+
+# Sum of values in the confusion matrix
+sum_confusion_values_all = np.sum(confusion_matrix(y, predictions_all))
+print(f"Sum of the values in the confusion matrix: {sum_confusion_values_all}")
+
+# Check if they're equal
+if total_rows == sum_confusion_values_all:
+    print("The total number of rows in the dataset and the sum of the values in the confusion matrix are equal.")
+else:
+    print("There is a discrepancy between the total number of rows in the dataset and the sum of the values in the confusion matrix.")
+
+# Step 4: ROC Curve and AUC
+probabilities = model.predict_proba(X_test)[:, 1]
+fpr, tpr, _ = roc_curve(y_test, probabilities, pos_label='Bad')
+plt.plot(fpr, tpr, color='blue', lw=2)
+plt.title("ROC Curve")
+plt.show()
+print("AUC:", roc_auc_score(y_test, probabilities))
+
+
+# Cross-validation
+cv_scores = cross_val_score(model, X, y, cv=3)
+print("Cross-validation scores:", cv_scores)
+
+# Step 5: Feature Ranking
+importances = model.feature_importances_
+sorted_indices = np.argsort(importances)[::-1]
+print("Top 10 Features:", X.columns[sorted_indices[:10]])
+
+# RF Run-Time Test
+positive_sample = X_test[y_test == 'Good'].iloc[0].values.reshape(1, -1)
+negative_sample = X_test[y_test == 'Bad'].iloc[0].values.reshape(1, -1)
+print("Positive Sample Prediction:", model.predict(positive_sample))
+print("Negative Sample Prediction:", model.predict(negative_sample))
+
+# Additional data checks
+original_shape = data.shape
+new_shape = X.shape
+print("Original Data Shape:", original_shape)
+print("New Data Shape:", new_shape)
+
+# Step 4: ROC Curve and AUC
+probabilities = model.predict_proba(X_test)[:, 1]
+fpr, tpr, _ = roc_curve(y_test, probabilities, pos_label='Bad')
+
+# Plot ROC curve
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = {:.2f})'.format(roc_auc_score(y_test, probabilities)))
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc='lower right')
+plt.show()
+
+
+################################################
+################################################
+############        OUTPUT          ############
+################################################
+################################################
+
+
+# No missing values in the dataset
+# RandomForestClassifier(max_features=10, n_estimators=1500, random_state=123)
+#               precision    recall  f1-score   support
+#
+#          Bad       0.79      0.74      0.77        31
+#         Good       0.72      0.78      0.75        27
+#
+#     accuracy                           0.76        58
+#    macro avg       0.76      0.76      0.76        58
+# weighted avg       0.76      0.76      0.76        58
+#
+# [[87  8]
+#  [ 6 90]]
+# Total number of rows in the dataset: 191
+# Sum of the values in the confusion matrix: 191
+# The total number of rows in the dataset and the sum of the values in the confusion matrix are equal.
+# AUC: 0.8602150537634409
+# Cross-validation scores: [0.828125   0.671875   0.71428571]
+# Top 10 Features: Index(['1.4.1_SP_ACS_BSRVSAN__ALLAREA__2019', '10.7.4_SM_POP_REFG_OR____2019',
+#        '8.4.2_EN_MAT_DOMCMPG____2019', '1.4.1_SP_ACS_BSRVH2O__ALLAREA__2019',
+#        '3.2.1_SH_DYN_MORT_BOTHSEX__<5Y_2019', '9.2.1_NV_IND_MANFPC____2019',
+#        '3.2.1_SH_DYN_MORT_MALE__<5Y_2019',
+#        '3.2.1_SH_DYN_MORT_FEMALE__<5Y_2019',
+#        '3.2.1_SH_DYN_IMRT_BOTHSEX__<1Y_2019', '17.13.1_PA_NUS_ATLS____2019'],
+#       dtype='object')
+# Positive Sample Prediction: ['Good']
+# Negative Sample Prediction: ['Good']
+# Original Data Shape: (191, 322)
+# New Data Shape: (191, 321)
